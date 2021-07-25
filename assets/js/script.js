eb2253218cb1599a2363a7487acf8b6922d0a777
lucawic <lucaswick@email.arizona.edu>
2021-07-25 10:29:29 -0700

    cards for daily weather and search bar added. Most of the necessary HTML is complete.

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+let openWeatherApiKey = "af04f08986988677da68ed9bdc572112";
+
+var apiUrl = "http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=60.99&lon=30.9&dt=1586468027&appid=" + openWeatherApiKey;
